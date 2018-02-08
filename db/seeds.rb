00726f3a5967bd3c0b8ff71e759ca74ae2d73e3a
Christian de Courcy <chris@hyper.no>
2018-02-08 19:53:40 +0100

    Added Moderator model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+moderator = Moderator.create( fullname: "Christian de Courcy", username: "chris@hyper.no", password: "!support" )
